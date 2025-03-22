b893e7cdab630feb1c8ec0fb5c35e3ff99941af3
MrFurqanPallawagau <dowloadpa@gmail.com>
2025-03-22 12:52:14 +0800

    commit

---
+++ komentar.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/*
+komentar multi varis, bisa untuk
+lebih dari satu baris
+*/
+function sayHello()
+{
+// ini komentar satu baris
+echo "hello" . PHP_EOL;
+}
+
+sayHello();
---
+++ string function.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(",",[10,11,12,13,14,15]));
+var_dump(explode(" ,","Eko Kurniawan Khaenedyy"));
+var_dump(strtolower("FURQANNNNNNN"));
+var_dump(strtoupper("furqan"));
+var_dump(trim("          EKO           "));
+var_dump((substr("FURQAN PALLAWAGAU", 0 ,3)));
